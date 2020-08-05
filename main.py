0b7dc4868f548498f237de5797925a82b8280570
Reboot93 <seerseer93@gmail.com>
2020-08-05 14:57:38 +0800

    test

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import tkinter as tk
+
+root = tk.Tk
+root.title('视频库清理助手 V0.1')
+
+control_window = tk.Tk
+control_window.title('请选择是否保留')
+control_window.wm_attributes('-topmost', 1)
+
+work_dir = []
+dont_dir = []
+output_dir = []
+many = int(0)
+
+
+def openPotplayer(filename):
+    global work_dir
+    global dont_dir
+    global output_dir
+    global many
+
+    os.system('C:\\"Program Files"\DAUM\PotPlayer\PotPlayerMini64.exe ' + filename)
+
+
+def killedPotplayer():
+    pass
+
+
+def mv():
+    pass
+
+
+def delete():
+    pass
+
+
+def main():
+    # 根据工作目录获取文件列表，是否遍历，等，获取文件目录
+    # 先处理工作目录下的
+    file_list = os.listdir(work_dir)
+
+    for filename in file_list:
+        if os.path.isfile(os.path.join(work_dir, filename)):
+            if filename[-3,] == 'mp4' & filename[-3,] == 'mkv':
+                openPotplayer(filename)
+                control_window.mainloop()
+                many = many + 1
