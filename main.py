6f29c93a79018275eec3370d275ac82de2018f23
Reboot93 <seerseer93@gmail.com>
2020-09-26 02:16:34 +0800

    实现基本功能/===/v0.1 =================== 已经勉强实现的功能： 指定文件夹 调用potplayer播放：mkv/mp4/avi 格式的视频 在播放视频时产生第二窗口，提供删除功能 （仅删除当前播放视频 和 文件名除后缀外与之相同的文件） =================== 存在的问题： 全是问题 显著的问题： 1.遍历视频播放过程中，如果需要退出，main.py会显示未响应，且继续遍历视频播放，需要强制关闭。 2.GUI

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import os
+import sys
+import threading
 import tkinter as tk
 from tkinter import filedialog
 
-root = tk.Tk
+root = tk.Tk()
 root.title('视频库清理助手 V0.1')
-
-control_window = tk.Tk
-control_window.title('请选择是否保留')
-control_window.wm_attributes('-topmost', 1)
-
-work_dir = []
-dont_dir = []
-output_dir = []
 many = int(0)
+file_name = '0'
+now_dir = '0'
+now_name = 0
+
+
+def control():
+    control_window = tk.Tk()
+    control_window.title('请选择是否保留')
+    control_window.geometry('300x200')
+    control_window.wm_attributes('-topmost', 1)
+
+    delete_bt = tk.Button(control_window, text='删除此视频及同名文件', command=delete)
+
+    delete_bt.pack()
+
+    control_window.mainloop()
 
 
 def openPotplayer(filename):
@@ -33,32 +43,56 @@ def mv():
 
 
 def delete():
-    pass
+    os.remove(file_name)
+    for name in os.listdir(now_dir):
+        if name.find(now_name[:-3]) != -1 and (name[-3:] != 'mkv' or name[-3:] != 'mp4' or name[-3:] != 'avi'):
+            os.remove(str(now_dir) + '/' + str(name))
 
 def find():
     global work_dir
+    global now_dir
 
-    for root,dirs,files in os.walk(work_dir)
+    control_run = threading.Thread(target=control)
+    control_run.start()
+
+    for root, dirs, files in os.walk(work_dir):
+        now_dir = root
         for name in files:
             global many
-            filename = os.join(root,name)
-            if filename[-3,] == 'mp4' & filename[-3] == 'mkv' & filename[-3,] == 'avi':
+            global file_name
+            global now_name
+
+            filename = os.path.join(root, name)
+            if str(filename[-3:]) == 'mp4' or str(filename[-3:]) == 'mkv' or str(filename[-3:]) == 'avi':
+                now_name = name
+                file_name = filename
                 openPotplayer(filename)
-                control_window.mainloop()
                 many = many + 1
 
-def main():
-    # 根据工作目录获取文件列表，是否遍历，等，获取文件目录
-    # 先处理工作目录下的
-    global many
+
+# 根据工作目录获取文件列表，是否遍历，等，获取文件目录
+def set_dir():
+    global WorkDirPrint
     global work_dir
-
-    def set_dir():
-        work_dir = filedialog.askdirectory()
-
-    set_dir_bt = tk.Button(root,text = '设置工作目录',command = set_dir)
-    start_bt = tk.Button(root,text = '开始筛选视频文件',command = find)
+    work_dir = filedialog.askdirectory()
+    WorkDirPrint.insert(0, work_dir)
 
 
+def quit():
+    sys.exit()
 
 
+set_dir_bt = tk.Button(root, text='设置工作目录', command=set_dir)
+start_bt = tk.Button(root, text='开始筛选视频文件', command=find)
+quit_bt = tk.Button(root, text='结束', command=quit)
+
+WorkDirPrint = tk.Listbox(root)
+
+set_dir_bt.grid(row=0, column=1)
+WorkDirPrint.grid(row=0, column=0)
+start_bt.grid(row=1, column=0)
+quit_bt.grid(row=1, column=1)
+
+root.mainloop()
+
+# ===========================================================================================
