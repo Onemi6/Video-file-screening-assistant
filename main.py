a8e665dc3bcc661ab24cc9b498515c3845f46343
Reboot93 <seerseer93@gmail.com>
2020-09-27 00:36:14 +0800

    v0.1.0.1

--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 import os
 import sys
 import threading
+from time import sleep
 import tkinter as tk
 from tkinter import filedialog
 
 root = tk.Tk()
-root.title('视频库清理助手 V0.1')
+root.title('视频库清理助手 V0.1.0.1')
 many = int(0)
+quit1 = 0
+wait1 = 0
+continueY = 0
 file_name = '0'
 now_dir = '0'
 now_name = 0
 
 
 def control():
+    def wait_set():
+        global wait1
+        wait1 = 1
+
+    def quit_set():
+        global quit1
+        quit1 = 1
+        control_window.destroy()
+        killedPotplayer()
+
+    def continue_set():
+        global continueY
+        continueY = 1
+
     control_window = tk.Tk()
     control_window.title('请选择是否保留')
-    control_window.geometry('300x200')
+    control_window.geometry('250x100')
     control_window.wm_attributes('-topmost', 1)
 
     delete_bt = tk.Button(control_window, text='删除此视频及同名文件', command=delete)
+    quit_bt = tk.Button(control_window, text='停止筛选视频文件', command=quit_set)
+    wait_bt = tk.Button(control_window, text='暂停视频筛选', command=wait_set)
+    continue_bt = tk.Button(control_window, text='继续视频筛选', command=continue_set)
 
     delete_bt.pack()
+    quit_bt.pack()
+    wait_bt.pack()
+    continue_bt.pack()
 
     control_window.mainloop()
 
@@ -35,7 +59,7 @@ def openPotplayer(filename):
 
 
 def killedPotplayer():
-    pass
+    os.system('taskkill /f /im PotPlayerMini64.exe')
 
 
 def mv():
@@ -61,6 +85,9 @@ def find():
             global many
             global file_name
             global now_name
+            global wait1
+            global continueY
+            global quit1
 
             filename = os.path.join(root, name)
             if str(filename[-3:]) == 'mp4' or str(filename[-3:]) == 'mkv' or str(filename[-3:]) == 'avi':
@@ -68,6 +95,16 @@ def find():
                 file_name = filename
                 openPotplayer(filename)
                 many = many + 1
+                if wait1 == 1:
+                    while continueY == 0:
+                        sleep(0.1)
+                    wait1 = 0
+            if quit1 == 1:
+                break
+        else:
+            continue
+        break
+    sys.exit()
 
 
 # 根据工作目录获取文件列表，是否遍历，等，获取文件目录
@@ -84,14 +121,14 @@ def quit():
 
 set_dir_bt = tk.Button(root, text='设置工作目录', command=set_dir)
 start_bt = tk.Button(root, text='开始筛选视频文件', command=find)
-quit_bt = tk.Button(root, text='结束', command=quit)
+quit_bt = tk.Button(root, text='退出', command=quit)
 
 WorkDirPrint = tk.Listbox(root)
 
 set_dir_bt.grid(row=0, column=1)
 WorkDirPrint.grid(row=0, column=0)
-start_bt.grid(row=1, column=0)
-quit_bt.grid(row=1, column=1)
+start_bt.grid(row=0, column=2)
+quit_bt.grid(row=0, column=3)
 
 root.mainloop()
 
